64444705425fbce500e437790019bbeb11d496e3
Gerardo Grignoli <gerardog@gmail.com>
2020-01-28 17:49:48 -0300

    Recovered C# version: - original C# shim (shim.cs -> shim.exe) - new C shim (shim.c -> newshim.exe)

--- build.ps1
+++ build.ps1
@@ -6,6 +6,13 @@ $src = "$PSScriptRoot\src"
 New-Item -ItemType Directory -Path $build -ErrorAction SilentlyContinue | Out-Null
 Remove-Item "$build\*" -Recurse -Force | Out-Null
 
+# Build
+Write-Output 'Compiling shim.cs ...'
+& "$PSScriptRoot\packages\Microsoft.Net.Compilers\tools\csc.exe" /deterministic /platform:anycpu /nologo /optimize /target:exe /out:"$build\shim.exe" "$src\shim.cs"
+
+if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
+
+# Import Visual Studio 2017 MsBuild Env Vars
 cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat`" && set > %temp%\vcvars.txt"
 
 Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
@@ -14,12 +21,9 @@ Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
   }
 }
 
-# Build
 Write-Output 'Compiling shim.c ...'
-& cmd /c cl.exe /O1 /Fe"$build\shim.exe" "$src\shim.c" '2>&1'
-
+& cmd /c cl.exe /O1 /Fe"$build\newshim.exe" "$src\shim.c" '2>&1'
 if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
-
 # Checksums
 Write-Output 'Computing checksums ...'
 Get-ChildItem "$build\*" -Include *.exe,*.dll -Recurse | ForEach-Object {
